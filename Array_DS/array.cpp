32589a5a57e90ef0ae50d0784df1540f433f832f
modi295 <chamanmodi19@gmail.com>
2023-08-28 12:50:51 +0530

    modified:   Array_DS/array.cpp

--- Array_DS/array.cpp
+++ Array_DS/array.cpp
@@ -52,3 +52,103 @@ int removeDuplicates(vector<int>& nums) {   //remove duplicate from the array
 
     // std::rotate(nums.begin(),nums.begin()+nums.size()-k, nums.end());    method also used to rotate right [back to front]
     // std::rotate(nums.begin(),nums.begin()+k, nums.end());                method also used to rotate left  [front to back]
+
+
+     void moveZeroes(vector<int>& nums) {          //Move zero at the of the array
+        vector<int>a;
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0||nums[i]<0){
+                a.push_back(nums[i]);
+            }
+            else{
+                x++;
+            }
+        }
+        for(int i=0;i<x;i++){
+            a.push_back(0);
+        }
+        for (int i = 0; i < nums.size(); i++) {
+        swap(nums[i], a[i]);
+    }
+    }
+
+
+
+vector < int > sortedArray(vector < int > a, vector < int > b) { // union of array
+    // Write your code here
+    set<int> x;
+    for(int i=0;i<a.size();i++){
+        x.insert(a[i]);
+    }
+    for(int i=0;i<b.size();i++){
+        x.insert(b[i]);
+    }
+
+    vector<int>a;
+    for(auto it : x){
+		a.push_back(it);
+	}
+	return a;   
+   
+}
+
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m) //intersection of array
+{
+	// Write your code here.
+	vector<int>a;
+	set<int>x;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(arr1[i]==arr2[j]){
+				x.insert(arr1[i]);
+			}
+		}
+	}
+	for(auto it : x){
+		a.push_back(it);
+	}
+	return a;
+}
+
+// Input: nums = [0,1]
+// Output: 2
+// Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+ int missingNumber(vector<int>& nums) {    //missing number in array
+        int n=nums.size();
+        int sum=0;
+        int s2=n*(n+1)/2;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        return s2-sum;
+    }
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+int findMaxConsecutiveOnes(vector<int>& nums) {     //find consicutive 1's in array
+        int count=0,res=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                res=max(res,count);
+                count=0;
+                
+            }
+        }
+        res=max(count,res);
+        return res;
+    }    
+
+ int singleNumber(vector<int>& nums) {   //find the number that appear once in array
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+    }    
